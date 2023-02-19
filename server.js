c9e0238d79596e8abad84b641084529ad0323e28
Siddhartha Roy <52354524+SiddharthaRoy1@users.noreply.github.com>
2023-02-19 13:29:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-var dbUrl = 'mongodb+srv://Siddhartha123:tylerdurden@cluster0.d87nh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-
+var dbUrl = 'mongodb+srv://Siddhartha123:tylerdurden@cluster0.d87nh.mongodb.net/?retryWrites=true&w=majority'
+             
 var Message = mongoose.model('Message', {
     name: String,
     message: String
